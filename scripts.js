f65b7ecb01b1ca31d1a79117dbd2711a6b3adc71
M-Umwari <m.umwari@alustudent.com>
2024-04-10 20:17:16 +0200

    Added the popular tutorials api to script.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Quotes API
+
 $.ajax({
   url: "https://smileschool-api.alx-tools.com/quotes",
   method: "GET",
@@ -68,4 +70,110 @@ $.ajax({
   error: function (error) {
     console.log("Error:", error);
   },
+});
+
+//Popular Tutorials API
+
+$(document).ready(function () {
+  $.ajax({
+    url: "https://smileschool-api.alx-tools.com/popular-tutorials",
+    method: "GET",
+    success: function (response) {
+      const videos = response;
+      $("#popular-videos-loading").remove();
+      $("#popular-videos").append(
+        $(
+          '<div class="carousel slide card-carousel" data-ride="carousel">'
+        ).append(
+          $('<div class="carousel-inner d-flex">').append(
+            response.map((video) => {
+              return $(
+                '<div class="card border-0 carousel-item d-block m-0">'
+              ).append(
+                $(
+                  `<img src="${video.thumb_url}" width="288" height="224" class="card-img-top" alt="Card Image">`
+                ),
+                $('<div class="card-body px-0" style="border: none;">').append(
+                  $('<h5 class="card-title">').text(video.title),
+                  $('<p class="card-text" style="opacity: 0.7;">').text(
+                    video["sub-title"]
+                  )
+                ),
+                $('<div class="card-footer px-0 bg-white">').append(
+                  $('<div class="d-flex" style="gap: 20px;">').append(
+                    $(
+                      `<img src="${video.author_pic_url}" class="rounded-circle" width=30 height=30 alt="">`
+                    ),
+                    $('<p style="color: rgba(194, 113, 255, 1);">').text(
+                      video.author
+                    )
+                  ),
+                  $("<div>").append(
+                    Array(video.star)
+                      .fill(0)
+                      .map(() => {
+                        return $('<img src="images/star_on.png" alt="">');
+                      })
+                  )
+                )
+              );
+            })
+          ),
+          $(
+            '<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">'
+          ).append(
+            $('<span class="carousel-control-prev-icon" aria-hidden="true">'),
+            $('<span class="sr-only">').text("Previous")
+          ),
+          $(
+            '<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">'
+          ).append(
+            $('<span class="carousel-control-next-icon" aria-hidden="true">'),
+            $('<span class="sr-only">').text("Next")
+          )
+        )
+      );
+
+      let scrollPostion = 0;
+      $("#popular-videos .card-carousel .carousel-control-next").on(
+        "click",
+        function () {
+          const cardWidth = $(
+            "#popular-videos .card-carousel .carousel-inner  .carousel-item"
+          ).outerWidth();
+          if (scrollPostion >= cardWidth * response.length - cardWidth) {
+            return;
+          }
+          scrollPostion += cardWidth;
+          $("#popular-videos .card-carousel .carousel-inner").animate(
+            { scrollLeft: scrollPostion },
+            400
+          );
+        }
+      );
+
+      $("#popular-videos .card-carousel .carousel-control-prev").on(
+        "click",
+        function () {
+          if (scrollPostion <= 0) {
+            return;
+          }
+          const width = $(
+            "#popular-videos .card-carousel .carousel-inner"
+          ).width();
+          const cardWidth = $(
+            "#popular-videos .card-carousel .carousel-inner  .carousel-item"
+          ).outerWidth();
+          scrollPostion -= cardWidth;
+          $("#popular-videos .card-carousel .carousel-inner").animate(
+            { scrollLeft: scrollPostion },
+            400
+          );
+        }
+      );
+    },
+    error: function (error) {
+      console.error(error);
+    },
+  });
 });
